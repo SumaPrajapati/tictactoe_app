297c6f78590d05ea7c8d532f60359263104f2ced
Sumanlata Prajapati <sumap.dotnet@gmail.com>
2025-08-22 13:14:42 -0400

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Installing Nginx if not present..."
-sudo yum install -y nginx
-sudo cp -r * /var/www/html/
+set -e
+# Files have been copied by CodeDeploy per `files:` block.
+# You could add permissions or cache-busting here if needed.
+sudo find /usr/share/nginx/html -type f -print0 | xargs -0 --no-run-if-empty sudo chmod 0644
